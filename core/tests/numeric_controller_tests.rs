c97127d9e5c2ad8428443f1808030ae00f23ea3f
david <druiz0992@gmail.com>
2025-07-04 12:11:29 +0200

    test ilqr/ddp

--- core/tests/numeric_controller_tests.rs
+++ core/tests/numeric_controller_tests.rs
@@ -1,7 +1,10 @@
+use control_rs::controllers::ddp::DDPOptions;
+use control_rs::controllers::ddp::controller::DDP;
 use control_rs::controllers::qp_lqr::QPOptions;
 use control_rs::controllers::qp_mpc::{ConvexMpc, ConvexMpcOptions};
 use control_rs::controllers::riccati_lqr::{RiccatiLQROptions, RiccatiRecursion};
 use control_rs::controllers::{ConstraintTransform, Controller, ControllerOptions, QPLQR};
+use control_rs::cost::GenericCostOptions;
 use control_rs::cost::generic::GenericCost;
 use control_rs::physics::constants as c;
 use control_rs::physics::discretizer::RK4Numeric;
@@ -11,6 +14,8 @@ use control_rs::physics::traits::State;
 use control_rs::utils::Labelizable;
 use nalgebra::{DMatrix, dvector};
 use osqp::Settings;
+use rand_distr::{Distribution, Normal};
+use std::f64::consts::PI;
 use std::sync::Arc;
 use symbolic_services::symbolic::ExprRegistry;
 
@@ -24,12 +29,14 @@ enum ControllerType {
     Mpc,
     MpcULimitsAndNoise(f64, f64, Vec<f64>),
     MpcUXLimitsAndNoise(f64, f64, f64, f64, usize, Vec<f64>),
+    Ilqr,
+    Ddp,
 }
 
 type Sim<M> = BasicSim<M, RK4Numeric<M>>;
 type NumericController<M> = Box<dyn Controller<Sim<M>>>;
 
-fn numeric_controller_setup(controller_type: ControllerType) {
+fn numeric_controller_setup(controller_type: ControllerType, tol: f64) {
     let m = 1.0;
     let l = 0.3;
     let j = 0.2 * m * l * l;
@@ -211,11 +218,69 @@ fn numeric_controller_setup(controller_type: ControllerType) {
                     .unwrap(),
             )
         }
+        ControllerType::Ilqr => {
+            let state_ref = Quadrotor2DState::new(0.0, 1.0, PI, 0.0, 0.0, 0.0);
+            let options = GenericCostOptions::<Quadrotor2DState, Quadrotor2DInput>::new()
+                .set_reference_state_trajectory(&vec![state_ref.clone(); n_steps]);
+            let qn_matrix = DMatrix::<f64>::identity(6, 6) * 100.0;
+            let cost = GenericCost::new(q_matrix, qn_matrix, r_matrix, Some(options)).unwrap();
+            expected_trajectory[n_steps] = state_ref.clone();
+            // initialize u_ref with random noise
+            let normal = Normal::new(0.0, 1.0).unwrap();
+            let mut rng = rand::thread_rng();
+            let u_ref: Vec<_> = (0..n_steps - 1)
+                .map(|_| {
+                    let u1 = normal.sample(&mut rng);
+                    let u2 = normal.sample(&mut rng);
+                    Quadrotor2DInput::new(u1, u2)
+                })
+                .collect();
+            let general_options = ControllerOptions::<Sim<Quadrotor2D>>::default()
+                .set_x_ref(&[state_ref.clone()])
+                .set_u_ref(&u_ref)
+                .set_dt(dt)
+                .unwrap()
+                .set_time_horizon(sim_time)
+                .unwrap();
+            let ilqr_options = DDPOptions::<Sim<Quadrotor2D>>::default()
+                .set_general(general_options)
+                .set_ilqr_enable(true);
+            let controller = DDP::new_numeric(sim, Box::new(cost.clone()), ilqr_options).unwrap();
+            Box::new(controller)
+        }
+        ControllerType::Ddp => {
+            let state_ref = Quadrotor2DState::new(0.0, 1.0, PI, 0.0, 0.0, 0.0);
+            let options = GenericCostOptions::<Quadrotor2DState, Quadrotor2DInput>::new()
+                .set_reference_state_trajectory(&vec![state_ref.clone(); n_steps]);
+            let qn_matrix = DMatrix::<f64>::identity(6, 6) * 100.0;
+            let cost = GenericCost::new(q_matrix, qn_matrix, r_matrix, Some(options)).unwrap();
+            expected_trajectory[n_steps] = state_ref.clone();
+            // initialize u_ref with random noise
+            let normal = Normal::new(0.0, 1.0).unwrap();
+            let mut rng = rand::thread_rng();
+            let u_ref: Vec<_> = (0..n_steps - 1)
+                .map(|_| {
+                    let u1 = normal.sample(&mut rng);
+                    let u2 = normal.sample(&mut rng);
+                    Quadrotor2DInput::new(u1, u2)
+                })
+                .collect();
+            let general_options = ControllerOptions::<Sim<Quadrotor2D>>::default()
+                .set_x_ref(&[state_ref.clone()])
+                .set_u_ref(&u_ref)
+                .set_dt(dt)
+                .unwrap()
+                .set_time_horizon(sim_time)
+                .unwrap();
+            let ddp_options =
+                DDPOptions::<Sim<Quadrotor2D>>::default().set_general(general_options);
+            let controller = DDP::new_numeric(sim, Box::new(cost.clone()), ddp_options).unwrap();
+            Box::new(controller)
+        }
     };
 
     let (x_traj, u_traj) = controller.solve(&state_0).unwrap();
 
-    let tol = 1e-3;
     assert!(
         (x_traj.last().unwrap().to_vector() - expected_trajectory.last().unwrap().to_vector())
             .abs()
@@ -223,9 +288,10 @@ fn numeric_controller_setup(controller_type: ControllerType) {
             < tol
     );
     assert!(
-        (u_traj.last().unwrap().to_vector() - dvector!(0.5 * m * c::GRAVITY, 0.5 * m * c::GRAVITY))
-            .abs()
-            .sum()
+        (u_traj.last().unwrap().to_vector().abs()
+            - dvector!(0.5 * m * c::GRAVITY, 0.5 * m * c::GRAVITY))
+        .abs()
+        .norm()
             < tol
     );
 
@@ -251,71 +317,90 @@ fn numeric_controller_setup(controller_type: ControllerType) {
     }
 }
 
+const TOL: f64 = 1e-3;
 #[test]
 #[should_panic]
 fn test_qp_lqr_numeric() {
-    numeric_controller_setup(ControllerType::QpLqr);
+    numeric_controller_setup(ControllerType::QpLqr, TOL);
 }
 
 #[test]
 #[should_panic]
 fn test_qp_lqr_limits_numeric() {
-    numeric_controller_setup(ControllerType::QpLqrUlimits(
-        0.2 * c::GRAVITY,
-        0.6 * c::GRAVITY,
-    ));
+    numeric_controller_setup(
+        ControllerType::QpLqrUlimits(0.2 * c::GRAVITY, 0.6 * c::GRAVITY),
+        TOL,
+    );
 }
 
 #[test]
 fn test_ricatti_finite_numeric() {
-    numeric_controller_setup(ControllerType::RiccatiRecursionLQRFinite);
+    numeric_controller_setup(ControllerType::RiccatiRecursionLQRFinite, TOL);
 }
 
 #[test]
 fn test_ricatti_infinite_numeric() {
-    numeric_controller_setup(ControllerType::RiccatiRecursionLQRInfinite);
+    numeric_controller_setup(ControllerType::RiccatiRecursionLQRInfinite, TOL);
 }
 
 #[test]
 fn test_ricatti_finite_limits_numeric() {
-    numeric_controller_setup(ControllerType::RiccatiRecursionLQRFiniteULimitsAndNoise(
-        0.2 * c::GRAVITY,
-        0.6 * c::GRAVITY,
-        vec![0.0; 6],
-    ));
+    numeric_controller_setup(
+        ControllerType::RiccatiRecursionLQRFiniteULimitsAndNoise(
+            0.2 * c::GRAVITY,
+            0.6 * c::GRAVITY,
+            vec![0.0; 6],
+        ),
+        TOL,
+    );
 }
 
 #[test]
 fn test_ricatti_infinite_limits_numeric() {
-    numeric_controller_setup(ControllerType::RiccatiRecursionLQRInfiniteULimitsAndNoise(
-        0.2 * c::GRAVITY,
-        0.6 * c::GRAVITY,
-        vec![0.0; 6],
-    ));
+    numeric_controller_setup(
+        ControllerType::RiccatiRecursionLQRInfiniteULimitsAndNoise(
+            0.2 * c::GRAVITY,
+            0.6 * c::GRAVITY,
+            vec![0.0; 6],
+        ),
+        TOL,
+    );
 }
 
 #[test]
 fn test_mpc_numeric() {
-    numeric_controller_setup(ControllerType::Mpc);
+    numeric_controller_setup(ControllerType::Mpc, TOL);
 }
 
 #[test]
 fn test_mpc_limits_numeric() {
-    numeric_controller_setup(ControllerType::MpcULimitsAndNoise(
-        0.2 * c::GRAVITY,
-        0.6 * c::GRAVITY,
-        vec![0.0; 6],
-    ));
+    numeric_controller_setup(
+        ControllerType::MpcULimitsAndNoise(0.2 * c::GRAVITY, 0.6 * c::GRAVITY, vec![0.0; 6]),
+        TOL,
+    );
 }
 
 #[test]
 fn test_mpc_uxlimits_numeric() {
-    numeric_controller_setup(ControllerType::MpcUXLimitsAndNoise(
-        0.2 * c::GRAVITY,
-        0.6 * c::GRAVITY,
-        -0.2,
-        0.2,
-        2,
-        vec![0.0; 6],
-    ));
+    numeric_controller_setup(
+        ControllerType::MpcUXLimitsAndNoise(
+            0.2 * c::GRAVITY,
+            0.6 * c::GRAVITY,
+            -0.2,
+            0.2,
+            2,
+            vec![0.0; 6],
+        ),
+        TOL,
+    );
+}
+
+#[test]
+fn test_ilqr_numeric() {
+    numeric_controller_setup(ControllerType::Ilqr, 1e-1);
+}
+
+#[test]
+fn test_ddp_numeric() {
+    numeric_controller_setup(ControllerType::Ddp, 1e-1);
 }
